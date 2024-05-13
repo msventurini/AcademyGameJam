fbbf230f26a781b0c994158076967b6e19d2a50d
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2024-05-13 00:59:50 -0300

    organized app colors

--- AcademyGameJam/AcademyGameJam/Design/Tokens.swift
+++ AcademyGameJam/AcademyGameJam/Design/Tokens.swift
@@ -8,23 +8,9 @@
 import Foundation
 import UIKit
 
-struct Tokens {}
-
-extension Tokens {
-    // TODO: This shouldn't be like this.
-    static func green() -> UIColor {
-        let green1: UIColor = .init(red: 82/255, green: 255/255, blue: 0/255, alpha: 1)
-        let green2: UIColor = .init(red: 54/255, green: 169/255, blue: 0/255, alpha: 1)
-        let green3: UIColor = .init(red: 42/255, green: 111/255, blue: 10/255, alpha: 1)
-        
-        return [green1, green2, green3].randomElement()!
-    }
-    
-    static func yellow() -> UIColor {
-        let green1: UIColor = .init(red: 119/255, green: 88/255, blue: 29/255, alpha: 1)
-        let green2: UIColor = .init(red: 191/255, green: 132/255, blue: 18/255, alpha: 1)
-        let green3: UIColor = .init(red: 203/255, green: 167/255, blue: 96/255, alpha: 1)
-        
-        return [green1, green2, green3].randomElement()!
+enum Tokens {
+    enum Colors {
+        static var green: UIColor { [UIColor.green1, UIColor.green2, UIColor.green3].randomElement()! }
+        static var yellow: UIColor { [UIColor.yellow1, UIColor.yellow2, UIColor.yellow3].randomElement()! }
     }
 }
--- AcademyGameJam/AcademyGameJam/GameScene/Map/TileNode.swift
+++ AcademyGameJam/AcademyGameJam/GameScene/Map/TileNode.swift
@@ -10,7 +10,7 @@ import SpriteKit
 
 extension MapNode {
     class TileNode: SKSpriteNode {
-        private let colorGoal = Tokens.green()
+        private let colorGoal = Tokens.Colors.green
         
         public let maxPollen: Float
         public private(set) var pollen: Float = 0
@@ -26,7 +26,7 @@ extension MapNode {
             self.zPosition = -1
             self.anchorPoint = .zero
             self.colorBlendFactor = 1.0
-            self.color = Tokens.yellow()
+            self.color = Tokens.Colors.yellow
             self.name = "MapTile"
         }
         
